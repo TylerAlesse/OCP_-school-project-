502b5fc39f22bb046024dd6fc895f20c9226cd30
Tyler J. Alesse <TylerAlesse@mail.com>
2016-03-31 12:39:33 -0500

    documentation to comments
    changed the lines of documentation to comments, because I don’t know
    the difference

--- Item.swift
+++ Item.swift
@@ -6,7 +6,7 @@ import UIKit
 class Item: NSManagedObject
 {
 
-    /// Function to initialize a new Item
+    // Function to initialize a new Item
     convenience init(name: String, image: UIImage?, location: String, inManagedObjectContext managedObjectContext: NSManagedObjectContext)
     {
         let entity = NSEntityDescription.entityForName("Item", inManagedObjectContext: managedObjectContext)!
@@ -16,59 +16,57 @@ class Item: NSManagedObject
         self.location = location
     }
 //
-    /// Function to get all CoreData values
-    ///
-    /// - parameter managedObjectContext: CoreData Connection
-    ///
+    // Function to get all CoreData values
+    //
+    // - parameter managedObjectContext: CoreData Connection
+    //
     class func fetchAll(managedObjectContext: NSManagedObjectContext) -> [Item] {
-        let listagemCoreData             = NSFetchRequest(entityName: "Item")
+        let listagemCoreData = NSFetchRequest(entityName: "Item")
         
         // Sort alphabetical by field "name"
-        let orderByName = NSSortDescriptor(key: "name", ascending: true, selector: "caseInsensitiveCompare:")
+        let orderByName = NSSortDescriptor(key: "name", ascending: true, selector: #selector(NSString.caseInsensitiveCompare(_:)))
         listagemCoreData.sortDescriptors = [orderByName]
         
         // Get items from CoreData
         return (try? managedObjectContext.executeFetchRequest(listagemCoreData)) as? [Item] ?? []
     }
     
-    /// Function to search item by name
-    ///
-    /// - parameter name: Item name
-    /// - parameter managedObjectContext: CoreData Connection
-    ///
+    // Function to search item by name
+    //
+    // - parameter name: Item name
+    // - parameter managedObjectContext: CoreData Connection
+    //
     class func search(name: String, inManagedObjectContext managedObjectContext: NSManagedObjectContext) -> Item? {
-        let fetchRequest       = NSFetchRequest(entityName: "Item")
+        let fetchRequest = NSFetchRequest(entityName: "Item")
         fetchRequest.predicate = NSPredicate(format: "name = %@", name)
         
-        let result             = (try? managedObjectContext.executeFetchRequest(fetchRequest)) as? [Item]
+        let result = (try? managedObjectContext.executeFetchRequest(fetchRequest)) as? [Item]
         return result?.first
     }
     
-    /// Function to check duplicate item
-    ///
-    /// - parameter name: Item name
-    /// - parameter managedObjectContext: CoreData Connection
-    ///
+    // Function to check duplicate item
+    //
+    // - parameter name: Item name
+    // - parameter managedObjectContext: CoreData Connection
+    //
     class func checkDuplicate(name: String, inManagedObjectContext managedObjectContext: NSManagedObjectContext) -> Bool {
         return search(name, inManagedObjectContext: managedObjectContext) != nil
     }
     
-    /// Function to delete a item
-    ///
-    /// - parameter managedObjectContext: CoreData Connection
-    ///
+    // Function to delete a item
+    //
+    // - parameter managedObjectContext: CoreData Connection
+    //
     func destroy(managedObjectContext: NSManagedObjectContext) {
         managedObjectContext.deleteObject(self)
     }
     
-    /// Function to save CoreData values
-    ///
-    /// - parameter managedObjectContext: CoreData Connection
-    ///
+    // Function to save CoreData values
+    //
+    // - parameter managedObjectContext: CoreData Connection
+    //
     func save(managedObjectContext: NSManagedObjectContext) {
-        do {
-            try managedObjectContext.save()
-        }
+        do { try managedObjectContext.save() }
         catch {
             let nserror = error as NSError
             print("Error on save: \(nserror.debugDescription)")
